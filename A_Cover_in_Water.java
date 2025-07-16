69bfa2358141d6d75bada0469705c1c7ba89bc60
Lokesh Gile <lokeshgile14@gmail.com>
2025-07-16 10:58:43 +0530

    Minecraft water mechanism

---
+++ A_Cover_in_Water.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class  A_Cover_in_Water{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            String s = sc.next();
+            
+            System.out.println(minActionsRequired(n, s));
+        }
+    }
+
+    public static int minActionsRequired(int n, String s) {
+        int ans=0;
+        boolean three_cell=false;
+        for(int i=0;i<n;i++){
+            if(s.charAt(i) =='.' && i+1<n && s.charAt(i+1) =='.' && i+2<n && s.charAt(i+2) =='.'){
+                three_cell=true;
+                return 2;
+            }
+
+            if(s.charAt(i) =='.') ans++;
+        }
+
+
+        return ans;
+    }
+}
